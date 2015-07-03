9a9867350f975d8c8a1ba894bafe0927f8a98edc
panos <panos@almende.org>
2015-07-03 21:09:16 +0200

    dd

--- app/views/partial/header.blade.php
+++ app/views/partial/header.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <img class="img-responsive" src="img/profile.png" alt="">
+                    <img class="img-responsive" src="img/profile_new.png" alt="">
                     <div class="intro-text">
                         <span class="name">Panagiotis Chatzichristodoulou</span>
                         <hr class="star-light">
