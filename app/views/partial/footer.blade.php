10aee8fe672700a3a7ce61688192060b7c2e2dc9
panos <panos@almende.org>
2015-07-03 20:47:22 +0200

    added soc media

--- app/views/partial/footer.blade.php
+++ app/views/partial/footer.blade.php
@@ -11,19 +11,19 @@
                         <h3>Around the Web</h3>
                         <ul class="list-inline">
                             <li>
-                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                                <a href="https://www.facebook.com/panagiotis.hatzichristslave" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                                <a href="https://plus.google.com/u/1/117352091835930355276/posts" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                                <a href="https://twitter.com/PHadjichristodo" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                                <a href="https://www.linkedin.com/profile/view?id=284857752&trk=nav_responsive_tab_profile" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
                             </li>
                             <li>
-                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-wordpress"></i></a>
+                                <a href="http://non-parametric.blogspot.nl/" class="btn-social btn-outline"><i class="fa fa-fw fa-wordpress"></i></a>
                             </li>
                         </ul>
                     </div>
